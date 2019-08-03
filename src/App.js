a0f907b73748d90ac6b91150ccac25f6aa83026e
Balou9 <emmanuel.owosekun@gmail.com>
2019-08-03 11:40:50 +0200

    alignment

--- src/App.js
+++ src/App.js
@@ -4,40 +4,41 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="appHeader">
+      <img src={logo} className="appLogo" alt="logo" />
       <br/>
       <br/>
-      <h1 className="Rocket-Header">
-        Rocket Recruiting
-      </h1>
-      <h2 class>
-      Recruiting West African IT-Talents for European Businesses
-      </h2>
+      <h2 className="rocketHeader">Rocket Recruiting</h2>
+      <h3 className="rocketDescription">Recruiting West African Developers for European Businesses</h3>
       </header>
-      <div className="Instruction-Container">
-        <div className="Developer-Instruction">
-          <h2 className="Developer-Title">For Developers</h2>
-            <p className="Developer-Text">
+      <div className="titleContainer">
+        <div className="developerTitle"><h2>Developers</h2></div>
+        <div className="businessTitle"><h2>Businesses</h2></div>
+      </div>
+      <div className="instructionContainer">
+        <div className="developerInstruction">
+            <p className="developerText">
             Take the opportunity to apply for developer jobs in European IT companies.
             You're welcome to add links like Github or other code to convince us
             why you should be part of the developer pool.
             </p>
-          <a className="itTalentlink" href="mailto:ittalent-rocketrecruiting@gmail.com">
-          ittalent-rocketrecruiting@gmail.com
-          </a>
+
         </div>
-        <div className="Business-Instruction">
-          <h2 className="Business-Title">For Businesses</h2>
-            <p className="Business-Text">
+        <div className="businessInstruction">
+          <p className="businessText">
             Fill your developer vacancies with the best west african developers.
             Contact us for a suitable selection.
             </p>
-            <a className="businessLink" href="mailto:business-rocketrecruiting@gmail.com">
-            business-rocketrecruiting@gmail.com
-            </a>
         </div>
+      </div>
+      <div className="linkContainer">
+      <a className="developerLink" href="mailto:ittalent-rocketrecruiting@gmail.com">
+      ittalent-rocketrecruiting@gmail.com
+      </a>
+      <a className="businessLink" href="mailto:business-rocketrecruiting@gmail.com">
+      business-rocketrecruiting@gmail.com
+      </a>
       </div>
     </div>
   );
