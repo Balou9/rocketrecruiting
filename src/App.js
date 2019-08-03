0121bcdf4d3502db60c6eaa4f4a8e06ea9c8a212
Balou9 <emmanuel.owosekun@gmail.com>
2019-08-03 21:42:28 +0200

    space

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <header className="appHeader">
-      <img src={logo} className="appLogo" alt="logo" />
+        <div className="circles">
+          <div className="circle1">
+          </div>
+          <div className="circle2">
+          </div>
+        </div>
+      <br/>
       <br/>
       <br/>
       <h2 className="rocketHeader">Rocket Recruiting</h2>
       <h3 className="rocketDescription">Recruiting West African Developers for European Businesses</h3>
       </header>
-      <div className="titleContainer">
-        <div className="developerTitle"><h2>Developers</h2></div>
-        <div className="businessTitle"><h2>Businesses</h2></div>
-      </div>
-      <div className="instructionContainer">
-        <div className="developerInstruction">
-            <p className="developerText">
-            Take the opportunity to apply for developer jobs in European IT companies.
-            You're welcome to add links like Github or other code to convince us
-            why you should be part of the developer pool.
-            </p>
+        <div className="appContainer">
+          <div className="titleContainer">
+            <div className="developerTitle"><h2>Developers</h2></div>
+            <div className="businessTitle"><h2>Businesses</h2></div>
+          </div>
+          <div className="instructionContainer">
+            <div className="developerInstruction">
+                <p className="developerText">
+                Take the opportunity to apply for developer jobs in European IT companies.
+                You're welcome to add links like Github or other code to convince us
+                why you should be part of the developer pool.
+                </p>
 
+            </div>
+            <div className="businessInstruction">
+              <p className="businessText">
+                Fill your developer vacancies with the best west african developers.
+                Contact us for a suitable selection.
+                </p>
+            </div>
+          </div>
+          <div className="linkContainer">
+          <a className="developerLink" href="mailto:developer-rocketrecruiting@gmail.com">
+          developer-rocketrecruiting@gmail.com
+          </a>
+          <a className="businessLink" href="mailto:business-rocketrecruiting@gmail.com">
+          business-rocketrecruiting@gmail.com
+          </a>
+          </div>
         </div>
-        <div className="businessInstruction">
-          <p className="businessText">
-            Fill your developer vacancies with the best west african developers.
-            Contact us for a suitable selection.
-            </p>
-        </div>
-      </div>
-      <div className="linkContainer">
-      <a className="developerLink" href="mailto:ittalent-rocketrecruiting@gmail.com">
-      ittalent-rocketrecruiting@gmail.com
-      </a>
-      <a className="businessLink" href="mailto:business-rocketrecruiting@gmail.com">
-      business-rocketrecruiting@gmail.com
-      </a>
-      </div>
     </div>
   );
 }
