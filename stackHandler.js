4ec2b3e29eb1a4df5be5fe9799588405440c150e
Balou9 <emmanuel.owosekun@gmail.com>
2020-02-13 22:00:30 +0100

    handle children

--- stackHandler.js
+++ stackHandler.js
@@ -1,4 +1,9 @@
+// get child class name of parent // recursive
+// CTA to COI call of interest
+// this.state object containing records states of a session
+// add eventListener on forward and back browser action, this.state object
 class StackDeck {
+
   constructor() {
 
     this.deck = {
@@ -9,20 +14,26 @@ class StackDeck {
       helloLink : document.getElementsByClassName("helloContainer")[0]
     }
 
-    this.appHeader = document.getElementsByClassName("appHeader")[0];
+    this.appHeader = document.getElementsByClassName("appHeader")[0]
+
+    this.developerCallToAction = document.getElementsByClassName("serviceDeveloperCTA")[0]
+    this.companyCallToAction = document.getElementsByClassName("serviceCompanyCTA")[0]
+    this.developerSayHello = document.getElementsByClassName("developerHelloLinkCTA")[0]
+    this.companySayHello = document.getElementsByClassName("companyHelloLinkCTA")[0]
+
     this.deck.homeLink.style.display = "flex"
     this.deck.serviceLink.style.display = "none"
     this.deck.developerLink.style.display = "none"
     this.deck.companyLink.style.display = "none"
     this.deck.helloLink.style.display = "none"
 
-    this.links = Object.keys(this.deck)
-    this.len = this.links.length
+    this.menuLinkNames = Object.keys(this.deck)
+    this.len = this.menuLinkNames.length
 
     // this.state = function () {
     //   var result = []
     //   for (var i = 0; i < this.len; i++) {
-    //     result.push(this.deck[this.links[i]].style.display)
+    //     result.push(this.deck[this.menuLinkNames[i]].style.display)
     //   }
     //   return result
     //
@@ -30,22 +41,64 @@ class StackDeck {
 
     this.findActive = function () {
       for (var i = 0; i < this.len; i++) {
-        if (this.deck[this.links[i]].style.display === "flex")
+        if (this.deck[this.menuLinkNames[i]].style.display === "flex")
         return i
+      }
+    }
+
+    this._getChildClassName = function (parent) {
+      var children = document.getElementsByClassName(parent)[0].children
+
+      if (children) {
+        var childClassNames = []
+        for (var i = 0; i < children.length; i++) {
+          if (children[i].nodeName === "svg") {
+            childClassNames.push(children[i].className.baseVal)
+          } else {
+            childClassNames.push(children[i].className)
+          }
+        }
+      }
+      // return childClassNames
+      return childClassNames
+    }
+
+    this.getChildClassName = function (parent) {
+      if (typeof parent === "string") {
+        return this._getChildClassName(parent)
+      }
+      else {
+        return this._getChildClassName(...parent)
       }
     }
 
     this.handle = function (event) {
       var index = this.findActive()
-      // deactivate active deck
-      this.deck[this.links[index]].style.display = "none"
-
       var clicked = event.target.className
-      // activate clicked deck
-        if (this.deck[clicked]) {
-          this.deck[clicked].style.display = "flex"
-        }
 
+      this.deck[this.menuLinkNames[index]].style.display = "none"
+
+      // if clicked is in array of menuLinkNames and their children
+      if (this.menuLinkNames.includes(clicked)) {
+        this.deck[clicked].style.display = "flex"
+      } else {
+        // if clicked is in array of serviceDeveloperCTA and their children
+        if (clicked === this.developerCallToAction.className) {
+          this.deck.developerLink.style.display = "flex"
+        }
+        // if clicked is in array of serviceCompanyCTA and their children
+        if (clicked === this.companyCallToAction.className) {
+          this.deck.companyLink.style.display = "flex"
+        }
+        // if clicked is in array of developerHelloLinkCTA and their children
+        if (clicked === this.developerSayHello.className) {
+          this.deck.helloLink.style.display = "flex"
+        }
+        // if clicked is in array of companyHelloLinkCTA and their children
+        if (clicked === this.companySayHello.className) {
+          this.deck.helloLink.style.display = "flex"
+        }
+      }
       return this
     }
   }
@@ -54,5 +107,24 @@ class StackDeck {
 var stackdeck = new StackDeck()
 
 stackdeck.appHeader.addEventListener("click", function (e) {
+  stackdeck.handle(e)
+  var c = stackdeck.getChildClassName(e.target.className)
+  console.log(stackdeck.getChildClassName(c))
+  console.log(stackdeck.getChildClassName(e.target.className))
+})
+
+stackdeck.developerCallToAction.addEventListener("click", function (e) {
+  stackdeck.handle(e)
+})
+
+stackdeck.companyCallToAction.addEventListener("click", function (e) {
+  stackdeck.handle(e)
+})
+
+stackdeck.developerSayHello.addEventListener("click", function (e) {
+  stackdeck.handle(e)
+})
+
+stackdeck.companySayHello.addEventListener("click", function (e) {
   stackdeck.handle(e)
 })
