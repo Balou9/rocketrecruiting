0234dbe006886d7d93b47f82c20871eacb26ea1d
Balou9 <emmanuel.owosekun@gmail.com>
2020-04-03 00:00:56 +0200

    react setup

---
+++ nugget/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div class="animation">
+        <svg class="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+          <circle class="circleFrame1"></circle>
+          <circle class="circleFrame2"></circle>
+          <g class="group">
+            <circle class="circle1"></circle>
+            <circle class="circle2"></circle>
+          </g>
+        </svg>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
